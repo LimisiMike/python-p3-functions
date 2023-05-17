66a7eb07a8ece37ae6b7ac645506d1cba0658b8b
LimisiMike <michaellimisi7@gmail.com>
2023-05-17 03:22:04 +0300

    Completed assignment

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,25 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
+
+greet_programmer()
 
 def greet(name):
-    pass
+    print(f"Hello, {name}")
+
+greet("Naureen!")
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}")
+
+greet_with_default()
+greet_with_default("Mike")
 
 def add(num1, num2):
-    pass
+    return num1 + num2
+
+add(1,2)
 
 def halve(number):
-    pass
+    return number / 2
